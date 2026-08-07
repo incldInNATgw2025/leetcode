2493df1a108c27e3e528e2fed237ec1955447e9f
N@meNe3! <m5vqy1a2dr32s@tutamail.com>
2026-08-07 03:25:13 +0200

    update median-of-2-sorted-arrays

--- median_of_2_sorted_arrays.py
+++ median_of_2_sorted_arrays.py
@@ -1,5 +1,5 @@
 class Solution:
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
         A, B = nums1, nums2
         total = len(nums1) + len(nums2)
 
