5cbe04ed71e1d9979b6b058012d670e239315c63
N@meNe3! <m5vqy1a2dr32s@tutamail.com>
2026-08-25 04:58:53 +0200

    construct_string_from_bin_tree

---
+++ construct_string_from_bin_tree.py
@@ -0,0 +1,23 @@
+from typing import Optional
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def tree2str(self, root: Optional[TreeNode]) -> str:
+        res = []
+        def preorder(root):
+            if not root:
+                return
+            res.append("(")
+            res.append(str(root.val))
+            if not root.left and root.right:
+                res.append("()")
+            preorder(root.left)
+            preorder(root.right)
+            res.append(")")
+        preorder(root)
+        return "".join(res)[1:-1]
